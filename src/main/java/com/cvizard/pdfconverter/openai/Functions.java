03cd669b42e81c6cfc6f64a0876aa2ac6a875952
Paweł Solecki <pawelll.solecki@gmail.com>
2023-08-28 12:11:47 +0200

    edit functions String

--- src/main/java/com/cvizard/pdfconverter/openai/Functions.java
+++ src/main/java/com/cvizard/pdfconverter/openai/Functions.java
@@ -4,9 +4,11 @@ import com.cvizard.pdfconverter.model.Resume;
 import lombok.Value;
 
 public interface Functions {
+    String resumeFunctionName = "save_resume";
+    String resumeFunctionDescription = "Save save_resume into db";
 
-    Function<Resume> RESUME = Function.of("save_resume",
-            "Save save_resume into db",
+    Function<Resume> RESUME = Function.of(resumeFunctionName,
+            resumeFunctionDescription,
             Resume.class);
 
     @Value(staticConstructor = "of")
